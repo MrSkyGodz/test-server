bcc1b79828e2c98ebc07031b32540a0960a62d4c
Yunus Ahmet AKDAL <39703795+MrSkyGodz@users.noreply.github.com>
2020-01-14 04:56:11 +0300

    Long changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from sys import stdout
 import logging
 from flask_socketio import SocketIO
-from flask import Flask, render_template, Response, stream_with_context, request
+from flask import Flask, render_template, Response, stream_with_context, request, redirect
 from PIL import Image
 from camera import VideoCamera
 from flask_socketio import SocketIO
@@ -9,37 +9,58 @@ import os
 import io
 import base64
 import cv2
+import time
 import numpy as np
 
-
 app = Flask(__name__)
-socketio = SocketIO(app)
+
+PATH_TO_TEST_IMAGES_DIR = './images'
 
 
+@app.route('/submit',methods=['POST'])
+def submit():
+
+
+	img = request.files['image']  # get the image
+	f = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
+	img.save('%s/%s' % (PATH_TO_TEST_IMAGES_DIR, f))		
+	
+	return Response("%s saved" % f)
+
+'''
 def gen(camera):
 	while True:
 		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+'''
+def gen(camera):
+	while True:
+		a = os.listdir('./images')
+
+		frame = Image.open("./images/"+ a[-1]).tobytes()
 		yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 '''
-@app.route('/submit',methods=['POST','GET'])
-def submit():
-	image = request.args.get('image')
-	#dosya = open("deneme.txt","w")
-	#dosya.write(image)
-	#dosya.close
-	return render_template('index2.html')
+@app.route('/feed')
+def video_feed():
+    return Response(img, mimetype='multipart/x-mixed-replace; boundary=frame')
 '''
-	
+
+
+
 @app.route('/')
 def index():
+
     return render_template('index2.html')
 
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+	app.run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)), debug=True)
 	#socketio.run(app,host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
---
+++ dene.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import cv2
+from PIL import Image 
+ 
+# Gri tonlamalı resim yükleme
+img = cv2.imread('deneme.jpg', 0)
+import cv2
+
+a = os.listdir('./images')
+#frame = cv2.imread(a[-1]).tobytes()
+img = Image.open("./images/"+ a[-1])
+print(img)
