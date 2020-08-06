734283dd56f8137c2d6537aa0f2c29b29b68724a
MrSkyGodz <39703795+MrSkyGodz@users.noreply.github.com>
2020-08-06 16:23:07 +0300

    l

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def index():
 
 if __name__ == '__main__':
 	#app.run(host="192.168.1.112", port=int(os.environ.get("PORT", 8080)), debug=True)
-	#app.run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)), debug=True)
-	socketio.run(app,host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+	#socketio.run(app,host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
